334a02c6379b4c3a29693fd572c38e5aa2f44921
ron <ron@einfach.org>
2019-07-31 08:25:32 +0800

    quote all the things!!!

--- init.sh
+++ init.sh
@@ -5,22 +5,22 @@ set -e
 appSetup () {
 
 	# Set variables
-	DOMAIN=${DOMAIN:-SAMDOM.LOCAL}
-	DOMAINPASS=${DOMAINPASS:-youshouldsetapassword}
-	JOIN=${JOIN:-false}
-	JOINSITE=${JOINSITE:-NONE}
-	MULTISITE=${MULTISITE:-false}
-	NOCOMPLEXITY=${NOCOMPLEXITY:-false}
-	INSECURELDAP=${INSECURELDAP:-false}
-	DNSFORWARDER=${DNSFORWARDER:-NONE}
-	HOSTIP=${HOSTIP:-NONE}
+	DOMAIN="${DOMAIN:-SAMDOM.LOCAL}"
+	DOMAINPASS="${DOMAINPASS:-youshouldsetapassword}"
+	JOIN="${JOIN:-false}"
+	JOINSITE="${JOINSITE:-NONE}"
+	MULTISITE="${MULTISITE:-false}"
+	NOCOMPLEXITY="${NOCOMPLEXITY:-false}"
+	INSECURELDAP="${INSECURELDAP:-false}"
+	DNSFORWARDER="${DNSFORWARDER:-NONE}"
+	HOSTIP="${HOSTIP:-NONE}"
 	
-	LDOMAIN=${DOMAIN,,}
-	UDOMAIN=${DOMAIN^^}
-	URDOMAIN=${UDOMAIN%%.*}
+	LDOMAIN="${DOMAIN,,}"
+	UDOMAIN="${DOMAIN^^}"
+	URDOMAIN="${UDOMAIN%%.*}"
 
 	# If multi-site, we need to connect to the VPN before joining the domain
-	if [[ ${MULTISITE,,} == "true" ]]; then
+	if [[ "${MULTISITE,,}" == "true" ]]; then
 		/usr/sbin/openvpn --config /docker.ovpn &
 		VPNPID=$!
 		echo "Sleeping 30s to ensure VPN connects ($VPNPID)";
@@ -28,8 +28,8 @@ appSetup () {
 	fi
 
         # Set host ip option
-        if [[ "$HOSTIP" != "NONE" ]]; then
-		HOSTIP_OPTION="--host-ip=$HOSTIP"
+        if [[ "${HOSTIP}" != "NONE" ]]; then
+		HOSTIP_OPTION="--host-ip=${HOSTIP}"
         else
 		HOSTIP_OPTION=""
         fi
@@ -43,15 +43,15 @@ appSetup () {
 	# If the finished file isn't there, this is brand new, we're not just moving to a new container
 	if [[ ! -f /etc/samba/external/smb.conf ]]; then
 		mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
-		if [[ ${JOIN,,} == "true" ]]; then
-			if [[ ${JOINSITE} == "NONE" ]]; then
-				samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
+		if [[ "${JOIN,,}" == "true" ]]; then
+			if [[ "${JOINSITE}" == "NONE" ]]; then
+				samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL
 			else
-				samba-tool domain join ${LDOMAIN} DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site=${JOINSITE}
+				samba-tool domain join "${LDOMAIN}" DC -U"${URDOMAIN}\administrator" --password="${DOMAINPASS}" --dns-backend=SAMBA_INTERNAL --site="${JOINSITE}"
 			fi
 		else
-			samba-tool domain provision --use-rfc2307 --domain=${URDOMAIN} --realm=${UDOMAIN} --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass=${DOMAINPASS} ${HOSTIP_OPTION}
-			if [[ ${NOCOMPLEXITY,,} == "true" ]]; then
+			samba-tool domain provision --use-rfc2307 --domain="${URDOMAIN}" --realm="${UDOMAIN}" --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass="${DOMAINPASS}" "${HOSTIP_OPTION}"
+			if [[ "${NOCOMPLEXITY,,}" == "true" ]]; then
 				samba-tool domain passwordsettings set --complexity=off
 				samba-tool domain passwordsettings set --history-length=0
 				samba-tool domain passwordsettings set --min-pwd-age=0
@@ -66,12 +66,12 @@ appSetup () {
 			idmap config ${URDOMAIN}: range = 10000-20000\\n\
 			idmap config ${URDOMAIN}: backend = ad\
 			" /etc/samba/smb.conf
-		if [[ $DNSFORWARDER != "NONE" ]]; then
+		if [[ "${DNSFORWARDER}" != "NONE" ]]; then
 			sed -i "/\[global\]/a \
 				\\\tdns forwarder = ${DNSFORWARDER}\
 				" /etc/samba/smb.conf
 		fi
-		if [[ ${INSECURELDAP,,} == "true" ]]; then
+		if [[ "${INSECURELDAP,,}" == "true" ]]; then
 			sed -i "/\[global\]/a \
 				\\\tldap server require strong auth = no\
 				" /etc/samba/smb.conf
@@ -81,16 +81,16 @@ appSetup () {
 	else
 		cp /etc/samba/external/smb.conf /etc/samba/smb.conf
 	fi
-        
+
 	# Set up supervisor
 	echo "[supervisord]" > /etc/supervisor/conf.d/supervisord.conf
 	echo "nodaemon=true" >> /etc/supervisor/conf.d/supervisord.conf
 	echo "" >> /etc/supervisor/conf.d/supervisord.conf
 	echo "[program:samba]" >> /etc/supervisor/conf.d/supervisord.conf
 	echo "command=/usr/sbin/samba -i" >> /etc/supervisor/conf.d/supervisord.conf
-	if [[ ${MULTISITE,,} == "true" ]]; then
-		if [[ -n $VPNPID ]]; then
-			kill $VPNPID
+	if [[ "${MULTISITE,,}" == "true" ]]; then
+		if [[ -n "${VPNPID}" ]]; then
+			kill "${VPNPID}"
 		fi
 		echo "" >> /etc/supervisor/conf.d/supervisord.conf
 		echo "[program:openvpn]" >> /etc/supervisor/conf.d/supervisord.conf
